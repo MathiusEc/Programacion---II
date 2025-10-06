f98666ba5b740e9b3a556b02d418cc7db01d41e4
Mathías D. Castillo <mdcy1123@gmail.com>
2025-10-06 15:55:44 -0500

    Uso de metodos java

---
+++ Creación de Métodos/Metodos_Java/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /** Declaracion de Objetos */
+        Proveedor vendor1 = new Proveedor();
+        Scanner sc = new Scanner(System.in);
+
+        /** Lectura de Datos */
+        String ruc, razon, dir, deta;
+        System.out.println("Proveedor RUC: ");
+        ruc = sc.nextLine();
+        System.out.println();
+
+        System.out.println("Proveedor Razon Social: ");
+        razon = sc.nextLine();
+        System.out.println();
+
+        System.out.println("Proveedor Direccion: ");
+        dir = sc.nextLine();
+        System.out.println();
+
+        deta = vendor1.detallesProve(ruc,razon,dir);
+        //Se ingresa al metodo con las variables que contienen los datos
+
+        //Crear metodo que entre como parametro "deta" y lo imprima en una ventana
+        vendor1.mostrarMensaje(deta);
+
+        /** Imprimir Datos*/
+        System.out.println("Info Proveedor\n"+deta);
+        // Imrimir esto en una ventana, pero traerlo e las clases a ttraves de un metodo
+
+
+    }
+}
---
+++ Creación de Métodos/Metodos_Java/src/Proveedor.java
@@ -0,0 +1,81 @@
+import javax.swing.*;
+
+public class Proveedor {
+    /** Declaracion de Atributos*/
+    String RUC;
+    String razonSocial;
+    String direccion;
+
+    /** Decalrcion de Constructores y Destructores*/
+    // Los constructores son metodos especiales que se ejecutan al momento de crear un objeto
+    // y empiezan con la primera letra en mayuscula y no retornan ningun valor
+    public Proveedor() {
+    }
+
+    public Proveedor(String RUC, String razonSocial) {
+        this.RUC = RUC;
+        this.razonSocial = razonSocial;
+    }
+
+    public Proveedor(String RUC, String razonSocial, String direccion) {
+        this.RUC = RUC;
+        this.razonSocial = razonSocial;
+        this.direccion = direccion;
+    }
+
+    /** Metodos propios de Java */
+    // Set -> Asigna
+    // Get -> Obtiene
+
+
+    public String getRUC() {
+        return RUC;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setRUC(String RUC) {
+        this.RUC = RUC;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /** Meotodos propios del Programador */
+    // Un metodo es una accion que realiza un objeto
+    // puede o no retornar un valor
+    // puede o no recibir parametros
+    // puede o no modificar los atributos de un objeto
+
+    public String detallesProve(String ruc, String razonSocial, String direccion){
+        String detcom;
+        detcom = "El RUC del proveedor es: " +  ruc + "\nRazon Social del Proveedor es: " + razonSocial +
+                "\nLa Direccion del proveedor es: " + direccion;
+
+        return detcom;
+    }
+
+    public void mostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+
+    // Parametros por valor es cuando se pasa una copia del valor
+    // Parametros por referencia es cuando se pasa la direccion de memoria del valor
+    // En Java todos los parametros son por valor, pero los objetos se comportan como si
+    // fueran por referencia, ya que se pasa la direccion de memoria del objeto
+    // y cualquier cambio que se haga en el objeto dentro del metodo, se refleja en el
+    // objeto original
+
+}
+
