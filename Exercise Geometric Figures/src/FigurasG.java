ac38bd9cf34105c7a5a8f9978606b4eb394d17ea
Mathías D. Castillo <mdcy1123@gmail.com>
2025-10-11 13:23:39 -0500

    Logica completa

--- Exercise Geometric Figures/src/FigurasG.java
+++ Exercise Geometric Figures/src/FigurasG.java
@@ -1,42 +1,138 @@
+import javax.swing.*;
+
 public class FigurasG {
     /** Declaración de Atributos*/
-    private float lado;
+    private float lado1;
+    private float lado2;
+    private float lado3;
     private float radio;
+    private float base;
+    private float altura;
+    private float D;
+    private float d;
 
     /** Declarcion de Constructores y Destructores*/
-    public FigurasG() {
-
+    public float getLado1() {
+        return lado1;
     }
 
-    public FigurasG(float radio) {
-        this.radio = radio;
+    public float getLado2() {
+        return lado2;
     }
 
-    /** Metodos propios de Java */
-    public float getLado() {
-        return lado;
+    public float getLado3() {
+        return lado3;
     }
 
     public float getRadio() {
         return radio;
     }
 
-    public void setLado(float lado) {
-        this.lado = lado;
+    public float getBase() {
+        return base;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public float getD() {
+        return D;
+    }
+
+    public void setLado1(float lado1) {
+        this.lado1 = lado1;
+    }
+
+    public void setLado2(float lado2) {
+        this.lado2 = lado2;
+    }
+
+    public void setLado3(float lado3) {
+        this.lado3 = lado3;
     }
 
     public void setRadio(float radio) {
         this.radio = radio;
     }
 
-    /** Meotodos propios del Programador */
-    public float cuadradoP(float lado) {
-        float p;
-
-        p = lado*4;
-        System.out.println("Perimetro es igual a: " + p);
-
-        return p;
+    public void setBase(float base) {
+        this.base = base;
     }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public void setD(float d) {
+        D = d;
+    }
+
+    /** Meotodos propios del Programador */
+    public void cuadradoP(float lado1) {
+        float p;
+        p = lado1*4;
+        JOptionPane.showMessageDialog(null, "El perimetro del cuadrado es: " + p);
+    }
+    public void cuadradoA(float lado1) {
+        float a;
+        a = lado1*lado1;
+        JOptionPane.showMessageDialog(null, "El área del cuadrado es: " + a);
+    }
+
+    public void trianguloP(float lado1, float lado2, float lado3) {
+        float p;
+        p = lado1 + lado2 + lado3;
+        JOptionPane.showMessageDialog(null, "El perimetro del triangulo es: " + p);
+    }
+    public void trianguloA(float base, float altura) {
+        float a;
+        a = (base*altura)/2;
+        JOptionPane.showMessageDialog(null, "El área del triangulo es: " + a);
+    }
+
+    public void romboP(float lado1) {
+        float p;
+        p = lado1*4;
+        JOptionPane.showMessageDialog(null, "El perimetro del rombo es: " + p);
+    }
+    public void romboA(float D, float d) {
+        float a;
+        a = (D*d)/2;
+        JOptionPane.showMessageDialog(null, "El área del rombo es: " + a);
+    }
+
+    public void circuloP(float radio) {
+        float p;
+        p = (float) (2*Math.PI*radio);
+        JOptionPane.showMessageDialog(null, "El perimetro del circulo es: " + p);
+    }
+    public void circuloA(float radio) {
+        float a;
+        a = (float) (Math.PI*(radio*radio));
+        // Math.PI es una constante que representa el valor de pi
+        // pertenece al paquete java.lang.Math
+        JOptionPane.showMessageDialog(null, "El área del circulo es: " + a);
+    }
+
+    // Validaciones
+    public static boolean validarTriangulo(float l1, float l2, float l3) {
+        // Verificar desigualdad triangular
+        if (l1 + l2 > l3 && l1 + l3 > l2 && l2 + l3 > l1) {
+            return true;
+        } else {
+            System.out.println("Error: Los lados no forman un triángulo válido");
+            return false;
+        }
+    }
+    public static boolean validarRombo(float diagonalMayor, float diagonalMenor) {
+        if (diagonalMayor > diagonalMenor) {
+            return true;
+        } else {
+            System.out.println("Error: La diagonal mayor debe ser mayor que la diagonal menor");
+            return false;
+        }
+    }
+
 
 }
--- Exercise Geometric Figures/src/Main.java
+++ Exercise Geometric Figures/src/Main.java
@@ -1,11 +1,13 @@
 // Ejercicio 1
-/* Necesito calcular el aerea y el primetro de un triangulo, cuadrado, circulo, rombo*/
+/* Necesito calcular el aerea y el primetro de un triangulo, cuadrado, circulo, rombo
+   no se validan las entradas de caracteres especiales*/
 
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         /** Declaracion de Objetos */
         FigurasG triangulo = new FigurasG();
@@ -16,36 +18,108 @@ public class Main {
 
 
         /** Lectura de Datos */
-
-        System.out.println("Ingrese la figura que desea calcular el área y el preimetro");
-        System.out.println("1. Cuadrado");
-        System.out.println("2. Triangulo");
-        System.out.println("3. Rombo");
-        System.out.println("4. Circulo");
-        int opc = Integer.parseInt(sc.nextLine());
+        int opc;
+        boolean val;
         do {
+            System.out.println("1. Cuadrado");
+            System.out.println("2. Triangulo");
+            System.out.println("3. Rombo");
+            System.out.println("4. Circulo");
+            System.out.println("5. Salir");
+            opc = Integer.parseInt(sc.nextLine());
             switch (opc) {
                 case 1:
-                    System.out.println("Cuadrado");
-                    System.out.println("Ingrese el lado");
-
-                    float l = Integer.parseInt(sc.nextLine());
-                    cuadrado.cuadradoP(l);
-
+                    float l;
+                    do {
+                        System.out.println("============Cuadrado============");
+                        System.out.println("Ingrese el lado: ");
+                        l = Float.parseFloat(sc.nextLine());
+                        if (l <= 0) {
+                            System.out.println("El lado no puede ser negativo o cero");
+                        } else {
+                            cuadrado.cuadradoA(l);
+                            cuadrado.cuadradoP(l);
+                        }
+                    } while (l <= 0);
                     break;
+
                 case 2:
-                    System.out.println("Triangulo");
+                    float b, h, l1, l2, l3;
+                    do {
+                        System.out.println("============Triangulo============");
+                        System.out.println("Ingrese la base (Lado 1): ");
+                        b = Float.parseFloat(sc.nextLine());
+                        l1 = b; // Asigno el valor de la base a l1 para usarlo en el perimetro
+
+                        System.out.println("Ingrese la altura: ");
+                        h = Float.parseFloat(sc.nextLine());
+
+                        System.out.println("Ingrese el lado 2: ");
+                        l2 = Float.parseFloat(sc.nextLine());
+
+                        System.out.println("Ingrese el lado 3: ");
+                        l3 = Float.parseFloat(sc.nextLine());
+
+                        val = FigurasG.validarTriangulo(l1, l2, l3);
+                        if (val == false) {
+                            l1 = 0; l2 = 0; l3 = 0; // Reinicio los valores para que el ciclo se repita
+                        }else {
+                            if (b <= 0 || h <= 0 || l1 <= 0 || l2 <= 0 || l3 <= 0) {
+                                System.out.println("Los valores no pueden ser negativos o cero");
+                            } else {
+                                triangulo.trianguloA(b, h);
+                                triangulo.trianguloP(l1, l2, l3);
+                            }
+                        }
+                    } while (b <= 0 || h <= 0 || l1 <= 0 || l2 <= 0 || l3 <= 0);
                     break;
+
                 case 3:
-                    System.out.println("Rombo");
+                    float D, d, lado = 0;
+                    do {
+                        System.out.println("============Rombo============");
+                        System.out.println("Ingrese la diagonal mayor: ");
+                        D = Float.parseFloat(sc.nextLine());
+
+                        System.out.println("Ingrese la diagonal menor: ");
+                        d = Float.parseFloat(sc.nextLine());
+
+                        val = FigurasG.validarRombo(D, d);
+                        if (val == false) {
+                            D = 0; d = 0; // Reinicio los valores para que el ciclo se repita
+                        }else{
+                            System.out.println("Ingrese el lado: ");
+                            lado = Float.parseFloat(sc.nextLine());
+
+                            if (D <= 0 || d <= 0 || lado <= 0) {
+                                System.out.println("Los valores no pueden ser negativos o cero");
+                            } else {
+                                rombo.romboA(D, d);
+                                rombo.romboP(lado);
+                            }
+                        }
+                    } while (D <= 0 || d <= 0 || lado <= 0);
                     break;
                 case 4:
-                    System.out.println("Circulo");
+                    float r;
+                    do {
+                        System.out.println("============Círculo============");
+                        System.out.println("Ingrese el radio: ");
+                        r = Float.parseFloat(sc.nextLine());
+
+                        if (r <= 0) {
+                            System.out.println("El radio no puede ser negativo o cero");
+                        } else {
+                            círculo.circuloA(r);
+                            círculo.circuloP(r);
+                        }
+                    } while (r <= 0);
+                    break;
+                case 5:
+                    JOptionPane.showMessageDialog(null, "Gracias por usar el programa");
                     break;
 
             }
-        }while (opc != 4);
-
-
+        } while (opc != 5);
     }
 }
