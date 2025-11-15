6d62da234ad83921a3762d9d6e7c918e2192c886
Mathías D. Castillo <mdcy1123@gmail.com>
2025-11-15 12:59:01 -0500

    exercise 4 of POO herency classes

---
+++ POO Herencia/Ejecicio 4/src/udla/mcastillo/ejercicio4/main/Banco.java
@@ -0,0 +1,32 @@
+package udla.mcastillo.ejercicio4.main;
+
+/*
+El banco El Ahorrador Feliz requiere la construcción de un sistema que le permita controlar las cuentas de sus
+clientes, y para ello las clasifica en dos tipos: cuentas de ahorros y cuentas corrientes.
+
+Todas las cuentas del banco tienen los siguientes datos:
+- Número de cuenta (dato numérico).
+- Nombre del cliente (cadena).
+- Saldo (dato numérico con fracción decimal).
+
+Con cada cuenta se pueden realizar las siguientes operaciones:
+- Consultar datos: a través de sus propiedades.
+- Depositar: incrementa el saldo con la cantidad de dinero que se deposita.
+- Retirar: antes de hacer el retiro, se debe verificar la suficiencia de saldo y, en caso de aprobarlo,
+  se disminuye el saldo.
+
+Las cuentas de ahorros presentan las siguientes características:
+- Porcentaje de interés mensual.
+- Método para depositar los intereses el primer día de cada mes.
+- Solamente se puede retirar un monto menor o igual al saldo.
+
+Las cuentas corrientes presentan las siguientes características:
+- Cobro del 4x1.000 en cada transacción.
+- Se puede sobregirar hasta en $300.000 cuando el saldo no es suficiente para el retiro.
+*/
+
+public class Banco {
+    public static void main(String[] args) {
+
+    }
+}
